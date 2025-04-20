2bc61bdb5e5ae13b014504d275e7b937ba538606
Vivian-n <L_qirui@163.com>
2025-04-20 12:14:19 +0800

    初始化

---
+++ pages/demo1/demo1.vue
@@ -0,0 +1,28 @@
+<template>
+	<!-- view类似传统div -->
+	<view hover-class="boxHover1" hover-start-time="2000" hover-stay-time="4000">
+		demo1
+		<view hover-stop-propagation="true  "  hover-class="boxHover" hover-start-time="2000" hover-stay-time="4000">
+			child
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.boxHover1{
+	background-color: lemonchiffon;
+}
+.boxHover{
+	background-color: lightcoral;
+}
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<navigator url="/pages/demo1/demo1">demo1</navigator>
+		<navigator url="/pages/index/index">index</navigator>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
